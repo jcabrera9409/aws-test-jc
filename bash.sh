df1b1267af9ad76774bf768945f9ce332d16d45e
Jose Cabrera <jcabrera9409@gmail.com>
2025-03-29 15:08:07 -0500

    .

--- bash.sh
+++ bash.sh
@@ -3,12 +3,14 @@ export OTEL_SERVICE_NAME="api-alumnos"
 
 export OTEL_TRACES_EXPORTER="otlp"
 export OTEL_PROPAGATORS="tracecontext,baggage"
-export OTEL_TRACES_SAMPLER="parentbased_traceidratio=1.0"
+export OTEL_TRACES_SAMPLER="parentbased_traceidratio"
+export OTEL_TRACES_SAMPLER_ARG="0.1"
 
 export OTEL_METRICS_EXPORTER="none"
 export OTEL_LOGS_EXPORTER="none"
 
 export OTEL_EXPORTER_OTLP_ENDPOINT="localhost:4317"
+
 
 export JAVA_TOOL_OPTIONS="-javaagent:opentelemetry-javaagent.jar"
 
