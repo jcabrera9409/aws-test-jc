8b279e16abbd66dddd9b90e1a3be64710698e291
Jose Cabrera <jcabrera9409@gmail.com>
2025-03-29 18:01:25 -0500

    se agrega metodo de busqueda por id

--- src/main/java/com/surrender/colegio/controller/AlumnoController.java
+++ src/main/java/com/surrender/colegio/controller/AlumnoController.java
@@ -1,11 +1,13 @@
 package com.surrender.colegio.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +26,15 @@ public class AlumnoController {
 		List<Alumno> lista = service.listar();
 		return new ResponseEntity<List<Alumno>>(lista, HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> listarPorId(@PathVariable int id) throws Exception {
+		Optional<Alumno> opt = service.listarPorId(id);
+		if(!opt.isPresent()) {
+			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Alumno>(opt.get(), HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/surrender/colegio/service/AlumnoService.java
+++ src/main/java/com/surrender/colegio/service/AlumnoService.java
@@ -1,6 +1,7 @@
 package com.surrender.colegio.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,9 @@ public class AlumnoService {
 	
 	public List<Alumno> listar() throws Exception {
 		return repo.findAll();
+	}
+	
+	public Optional<Alumno> listarPorId(int id) throws Exception {
+		return repo.findById(id);
 	}
 }
